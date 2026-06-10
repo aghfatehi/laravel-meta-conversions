3f803558e72ce6111f9832ff0adce32befde08e1
fatehi AL-AGHBARI <agh.fatehi@yahoo.com>
2026-06-10 14:41:08 +0300

    refactor: Update PixelComponentTest to use Pixel component for rendering

--- tests/Feature/PixelComponentTest.php
+++ tests/Feature/PixelComponentTest.php
@@ -1,10 +1,13 @@
 <?php
 
+use Aghfatehi\LaravelMetaConversions\View\Components\Pixel;
+
 it('renders pixel component with pixel id', function () {
     config()->set('facebook-conversion-service.pixel_id', 'TEST_PIXEL_123');
     config()->set('facebook-conversion-service.enabled', true);
 
-    $html = view('facebook-conversion::pixel')->render();
+    $component = new Pixel;
+    $html = view($component->render(), $component->data())->render();
 
     expect($html)->toContain('fbq("init", "TEST_PIXEL_123")');
     expect($html)->toContain('fbq("track", "PageView")');
@@ -14,32 +17,20 @@ it('does not render pixel when disabled', function () {
     config()->set('facebook-conversion-service.pixel_id', 'TEST_PIXEL_123');
     config()->set('facebook-conversion-service.enabled', false);
 
-    $html = view('facebook-conversion::pixel')->render();
+    $component = new Pixel;
+    $html = view($component->render(), $component->data())->render();
 
     expect($html)->not->toContain('fbq');
-});
-
-it('renders pixel view via include', function () {
-    config()->set('facebook-conversion-service.pixel_id', 'TEST_PIXEL_123');
-    config()->set('facebook-conversion-service.enabled', true);
-
-    $view = view('facebook-conversion::pixel')->render();
-
-    expect($view)->toContain('fbq("init", "TEST_PIXEL_123")');
-    expect($view)->toContain('fbq("track", "PageView")');
 });
 
 it('includes event id in pixel view when provided', function () {
     config()->set('facebook-conversion-service.pixel_id', 'TEST_PIXEL_123');
     config()->set('facebook-conversion-service.enabled', true);
 
-    $view = view('facebook-conversion::pixel', [
-        'eventName' => 'Purchase',
-        'eventId' => 'dedup-123',
-        'customData' => ['value' => 99.99, 'currency' => 'SAR'],
-    ])->render();
+    $component = new Pixel('Purchase', 'dedup-123', ['value' => 99.99, 'currency' => 'SAR']);
+    $html = view($component->render(), $component->data())->render();
 
-    expect($view)->toContain('dedup-123');
-    expect($view)->toContain('Purchase');
-    expect($view)->toContain('99.99');
+    expect($html)->toContain('dedup-123');
+    expect($html)->toContain('Purchase');
+    expect($html)->toContain('99.99');
 });
